d21a96d7cbe0c44e2ab3ebf8df0c05b6bf59b4c0
Clemens Rosenbaum <crosenbaum@asapp.com>
2020-01-22 16:34:50 -0500

    added more datasets: QQP, RTE, QNLI, WNLI

--- flambe/nlp/text_comparison/__init__.py
+++ flambe/nlp/text_comparison/__init__.py
@@ -1,4 +1,6 @@
-from flambe.nlp.text_comparison.datasets_nli import SNLIDataset, MultiNLIDataset, SCIDataset, WNLIDataset
+from flambe.nlp.text_comparison.datasets_nli import SNLIDataset, MultiNLIDataset, SCIDataset, WNLIDataset, \
+    QQPDataset, QNLIDataset, RTEDataset
 from flambe.nlp.text_comparison.model import DualTextClassifier
 
-__all__ = ['SNLIDataset', 'MultiNLIDataset', 'DualTextClassifier', 'SCIDataset', 'WNLIDataset']
+__all__ = ['SNLIDataset', 'MultiNLIDataset', 'DualTextClassifier', 'SCIDataset', 'WNLIDataset', 'QQPDataset',
+           'QNLIDataset', 'RTEDataset']
